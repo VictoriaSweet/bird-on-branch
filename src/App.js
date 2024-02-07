86a28277c36251f6cff5e5d102c065cf413cb926
VictoriaSweet <sweet.victoria1218@gmail.com>
2024-02-07 10:49:31 -0600

    added intial file set up and packages

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react'; 
+
+const App = () => {
+  return (
+    <div className="header">
+      <h1> Victoria's E-commerce site</h1>
+      <h2> Happy Browsing!</h2>
+    </div>
+  );
+};
+
+export default App;
